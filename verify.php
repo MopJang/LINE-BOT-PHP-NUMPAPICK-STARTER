1116314a67c79e4dd8a7e9bf62143a9f9c5ee5f4
MopJang <jingjang12@gmail.com>
2018-12-21 19:20:18 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '/gkTqeHqRm/ZXGcu2uduTNwsyXxguamxSPyMGDdlReM+gM58IH2k2PaF7cszVSMzLnvoxGnu2FrVUqseM/RSwY4F/pUZqSbCjdAece2YmCHpAVznR/yNfpa3pWZVNoOvOn4wB1kOaM/Bdaf5sNBbgQdB04t89/1O/w1cDnyilFU=
+/*$access_token = '/gkTqeHqRm/ZXGcu2uduTNwsyXxguamxSPyMGDdlReM+gM58IH2k2PaF7cszVSMzLnvoxGnu2FrVUqseM/RSwY4F/pUZqSbCjdAece2YmCHpAVznR/yNfpa3pWZVNoOvOn4wB1kOaM/Bdaf5sNBbgQdB04t89/1O/w1cDnyilFU=
 ';
 
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -13,4 +13,5 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo $result;
+echo $result;*/
+<?php$access_token = '/gkTqeHqRm/ZXGcu2uduTNwsyXxguamxSPyMGDdlReM+gM58IH2k2PaF7cszVSMzLnvoxGnu2FrVUqseM/RSwY4F/pUZqSbCjdAece2YmCHpAVznR/yNfpa3pWZVNoOvOn4wB1kOaM/Bdaf5sNBbgQdB04t89/1O/w1cDnyilFU=';$url = 'https://api.line.me/v1/oauth/verify';$headers = array('Authorization: Bearer ' . $access_token);$ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);$result = curl_exec($ch);curl_close($ch);echo $result;
