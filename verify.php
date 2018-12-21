78a504fa228e2af872095f2bd4ffe9bbd129e3db
MopJang <jingjang12@gmail.com>
2018-12-21 19:45:24 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 $access_token = '/gkTqeHqRm/ZXGcu2uduTNwsyXxguamxSPyMGDdlReM+gM58IH2k2PaF7cszVSMzLnvoxGnu2FrVUqseM/RSwY4F/pUZqSbCjdAece2YmCHpAVznR/yNfpa3pWZVNoOvOn4wB1kOaM/Bdaf5sNBbgQdB04t89/1O/w1cDnyilFU=
 ';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
